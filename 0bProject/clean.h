eaf7ad49d67cb5259a40fb04d86d8509d154fd3f
Darya <33616442+lolilofit@users.noreply.github.com>
2018-09-13 22:53:44 +0700

    Create clean.h

---
+++ 0bProject/clean.h
@@ -0,0 +1,7 @@
+#include<string>
+
+namespace cleanstring {
+	std::string EraseChar(std::string arg, char ch, int pos);
+	std::string CleanString(std::string arg);
+	std::string ToLower(std::string arg);
+}
