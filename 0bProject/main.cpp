6b336024cc09aae3242a9fb68e04f290743310a6
Darya <33616442+lolilofit@users.noreply.github.com>
2018-09-22 21:18:33 +0700

    Update main.cpp

--- 0bProject/main.cpp
+++ 0bProject/main.cpp
@@ -1,228 +1,100 @@
 #include<iostream>
-#include<algorithm>
-#include<cassert>
+#include<string>
+#include<fstream>
+#include<map>
 #include<vector>
-#include<unordered_map>
-
-enum class Trit : unsigned int { False = 1, Unknown = 0, True = 3 };
+#include<algorithm>
+#include <iomanip>
+#include"clean.h"
 
 using namespace std;
 
+class words {
 
-Trit operator &(Trit &first, Trit &second) {
-	if (first == Trit::False || second == Trit::False)
-		return Trit::False;
-	if (first == Trit::True && second == Trit::True)
-		return Trit::True;
-	return Trit::Unknown;
-}
-
-Trit operator |(Trit &first, Trit &second) {
-	if (first == Trit::True || second == Trit::True)
-		return Trit::True;
-	if (first == Trit::False && second == Trit::False)
-		return Trit::False;
-	return Trit::Unknown;
-
-}
-
-Trit operator ~(Trit &elem) {
-	if (elem == Trit::True)
-		return Trit::False;
-	if (elem == Trit::False)
-		return Trit::True;
-	return Trit::Unknown;
-
-}
-
-
-
-
-class TritArr
-{
 	int count = 0;
-public:
-	vector <unsigned int> arr;
+	map <string, int> WordsList;
+    
+	void SortOut(ofstream &out) {
 
-public:
-	TritArr(int length) {
-		assert(length >= 0);
-
-		for (int i = 0; i < length; i++)
-			arr.push_back(0);
-		count = length;
-	}
-
-
-	~TritArr() {
-		arr.clear();
-	}
-
-	void Trim(int last_index) {
-		this->arr.resize(last_index);
-	}
-
-	int LastUnknown() {
-		for (int i = this->arr.capacity(); i > 0; i--) {
-			for (int j = 3; j >= 0; j--) {
-				if (this->read(i * 4 + j) > 0)
-					return 4 * i + j + 1;	
-			}
+		multimap<int, string> SortMap;
+		//make sorted map
+		for (auto cur = WordsList.begin(); cur != WordsList.end(); ++cur)
+			SortMap.insert(SortMap.end(), make_pair(cur->second, cur->first));
+		for (auto cur = SortMap.rbegin(); cur != SortMap.rend(); ++cur) {
+			double persent = (100.000*cur->first) / count;
+			out << cur->second << " " << cur->first << " " << fixed << setprecision(3) << persent << "%" << endl;
 		}
-		return -1;
+
+		return;
 	}
 
-	int cardinality(Trit val) {
-		int count_val = 0;
-		if (static_cast<int> (val) != 0) {
-			for (int i = this->arr.capacity(); i > 0; i--) {
-				for (int j = 3; j >= 0; j--) {
-					if (this->read(i * 4 + j) == static_cast<int> (val))
-						count_val++;
-				}
-			}
-		}
+	void MakeMap(string arg) {
+		//if there is no element with this word in a map
+		if (WordsList.find(arg) == WordsList.end())
+			WordsList.insert(WordsList.end(), make_pair(arg, 1));
 		else
-			count_val = this->arr.capacity() - this->LastUnknown();
-		return count_val;
-	}
+			//if we have an element with this word in a map
+			(WordsList.find(arg))->second++;
 
-	void Shrink() {
-		for (int i = this->arr.capacity(); i > count; i--) {
-			if (this->arr[i] > 0) {
-				for (int j = 3; j >= 0; j--) {
-					if (this->read(i * 4 + j) == 1 || this->read(i * 4 + j) == 2) {
-						this->arr.resize(4 * i + j);
-						return;
-					}
-				}
-			}
+		count++;
+		return;
+	}
+public:
+	int CountWords(ifstream &in, ofstream &out) {
+		
+		while (!in.eof()) {
+			string arg;
+			//get one word
+			getline(in, arg, ' ');
+			//translate all symbols from the word to small register
+			//and delete all punctuation marks
+			arg = cleanstring::ToLower(arg);
+			arg = cleanstring::CleanString(arg);
+			if ((arg[0] != '\n') && (arg.size() != 0))
+				//create a map with words and their frequency
+				MakeMap(arg);
 		}
-		this->arr.resize(count);
-	}
 
-	int read(int pos) {
-		if (this->arr.capacity() < pos)
-			return 0;
-		unsigned int point = this->arr[pos / 4];
-		return static_cast<int> (((point >> (6 - 2 * (pos % 4))) & 1) + 2 * (1 & (point >> (7 - 2 * (pos % 4)))));
-	}
+		//put wors and their frequense in descending order to the csv file 
+		SortOut(out);
+		return 0;
 
-	void SetTrit(int pos, Trit val) {
-		if (this->arr.capacity()*4 < pos) {
-			if (val == Trit::Unknown)
-				return;
-			if (val == Trit::True) {
-				this->arr.resize(pos / 4);
-				this->arr[pos / 4] = this->arr[pos / 4] & 1;
-			}
-		}
-		else {
-			int num = static_cast <int> (val);
-			this->arr[pos / 4] = (this->arr[pos / 4] & (~(1 << (7 - 2 * (pos % 4))))) | (num & 2);
-			this->arr[pos / 4] = (this->arr[pos / 4] & (~(1 << (6 - 2 * (pos % 4))))) | (num & 1);
-		}
-	}
-
-	TritArr operator[](int pos) {
-		this->arr.push_back(pos);
-		this->arr.push_back(this->read(pos));
-		return *this;
-	}
-
-	void operator=(Trit val) {
-		this->SetTrit(this->arr[arr.capacity() - 2], val);
-	    this->arr.erase(this->arr.begin() + this->arr.capacity() - 2, this->arr.end());
 	}
 
 };
 
-bool operator==(TritArr trit_arr, Trit val) {
-	bool res = 0;
-	if (static_cast<int>(val) == trit_arr.arr[trit_arr.arr.capacity()-1])
-		res = 1;
-	trit_arr.arr.erase(trit_arr.arr.begin()+ trit_arr.arr.capacity() - 2, trit_arr.arr.end());
-	return res;
-}
-
-
-TritArr operator&(TritArr &first, TritArr &second) {
-	int minimum = min(first.arr.capacity(), second.arr.capacity());
-	TritArr res_arr(minimum);
-
-	for (int i = 0; i < minimum; i++) {
-		for (int j = 0; j < 4; j++) {
-			int first_trit = first.read(i * 4 + j);
-			int	second_trit = second.read(i * 4 + j);
-
-			Trit new_tritf = static_cast <Trit> (first_trit);
-			Trit new_trits = static_cast <Trit> (second_trit);
-			Trit res_trit = new_tritf & new_trits;
-
-			res_arr.SetTrit(i * 4 + j, res_trit);
-		}
-	}
-	return res_arr;
-}
-
-TritArr operator|(TritArr &first, TritArr &second) {
-	int maximum = max(first.arr.capacity(), second.arr.capacity());
-	TritArr res_arr(maximum);
-
-	for (int i = 0; i < maximum; i++) {
-		for (int j = 0; j < 4; j++) {
-			int first_trit = first.read(i * 4 + j);
-			int	second_trit = second.read(i * 4 + j);
-
-			Trit new_tritf = static_cast <Trit> (first_trit);
-			Trit new_trits = static_cast <Trit> (second_trit);
-			Trit res_trit = new_tritf | new_trits;
-
-			res_arr.SetTrit(i * 4 + j, res_trit);
-		}
-	}
-	return res_arr;
-}
-
-TritArr operator~(TritArr &first) {
-
-	TritArr res_arr(first.arr.capacity());
-	for (int i = 0; i < first.arr.capacity(); i++) {
-		for (int j = 0; j < 4; j++) {
-			int first_trit = first.read(i * 4 + j);
-
-			Trit new_tritf = static_cast <Trit> (first_trit);
-			Trit res_trit = ~new_tritf;
-			res_arr.SetTrit(i * 4 + j, res_trit);
-		}
-	}
-	return res_arr;
-}
-
-/*
- int cardinality(unordered_map<Trit, int, hash<int>> trit_map, Trit val) {
-	if(static_cast <int> (val) != 0)
-    	return trit_map.count(val);
-	int count_val = 0, i =0;
-	for (auto item : trit_map) {
-		i++;
-		if (item.first != static_cast <Trit> (0))
-			count_val = i+1;
-	}
-	return (trit_map.size() - count_val);
-} 
-*/
-
-int main() {
-	TritArr ar1(3);
-	TritArr ar2(2);
-	ar1.arr[0] = 3;
-	ar1.arr[1] = 3;
-	ar1.arr[2] = 2;
+void OpenIn(const string InputFile, ifstream &in) {
 	
+	in.open(InputFile);
+	if (!in.is_open()) 
+		cerr << "no such file";
+	return;
+}
 
-	ar2.arr[1] = 0;
-	ar2.arr[0] = 1;
+void OpenOut(const string OutFile, ofstream &out) {
+	out.open(OutFile);
+	if (!out.is_open()) 
+		cerr << "no such file";
+	return;
+}
 
+int main(int argc, const char *argv[]) {
+	ifstream in;
+	ofstream out;
+	if (argc == 3) {
+		OpenIn(argv[1], in);
+		//in.open(argv[1]);
+	    OpenOut(argv[2], out);
+		//out.open(argv[2]);
+	}
+	else {
+		cout << "wrong number of arguments";
+		return 0;
+	}
+	
+	words st;	
+    st.CountWords(in, out);
+	in.close();
+	out.close();
 	return 0;
 }
