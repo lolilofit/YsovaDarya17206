cd39ee86dcd83c3f71b466d05ced1df356a43bb3
Darya <33616442+lolilofit@users.noreply.github.com>
2018-10-02 15:59:53 +0700

    Update OneTrit.h

--- OOPlab1/OneTrit.h
+++ OOPlab1/OneTrit.h
@@ -3,8 +3,6 @@
 
 enum class Trit : unsigned int { False = 1, Unknown = 0, True = 3 };
 
-Trit set(Trit val);
-
 Trit operator &(Trit first, Trit second);
 
 Trit operator |(Trit first, Trit second);
