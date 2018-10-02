ae8e6ab047c4faa863baf15a29d01962a8a6d58b
Darya <33616442+lolilofit@users.noreply.github.com>
2018-10-02 15:57:25 +0700

    Update LogicOp.cpp

--- OOPlab1/LogicOp.cpp
+++ OOPlab1/LogicOp.cpp
@@ -3,63 +3,76 @@
 #include<iostream>
 #include<algorithm>
 
+//logic operators for set of trits
 
-TritArr operator&(TritArr &first, TritArr &second) {
-	const int size_t = sizeof(unsigned int) * 4;
-
+TritSet operator&(TritSet &first, TritSet &second) {
+	const int size = sizeof(unsigned int) * 4;
+	
+	//size of new trit
 	int minimum = std::min(first.arr.size(), second.arr.size());
-	TritArr res_arr(std::min(first.capacity(), second.capacity()));
+	TritSet res_arr(std::min(first.capacity(), second.capacity()));
 
 	for (int i = 0; i < minimum; i++) {
-		for (int j = 0; j < size_t; j++) {
-			int first_trit = first.read(i * size_t + j);
-			int	second_trit = second.read(i * size_t + j);
+		for (int j = 0; j < size; j++) {
+			//read each trit
+			int first_trit = first.read(i * size + j);
+			int	second_trit = second.read(i * size + j);
 
+			//make new trit which is a result of operation
 			Trit new_tritf = static_cast <Trit> (first_trit);
 			Trit new_trits = static_cast <Trit> (second_trit);
 			Trit res_trit = new_tritf & new_trits;
 
-			res_arr.SetTrit(i * size_t + j, res_trit);
+			//write this trit to the new set of trits
+			res_arr.SetTrit(i * size + j, res_trit);
 		}
 	}
 	return res_arr;
 }
 
-TritArr operator|(TritArr &first, TritArr &second) {
+TritSet operator|(TritSet &first, TritSet &second) {
 	const int size_t = sizeof(unsigned int) * 4;
 	
+	//size of new trit
 	int maximum = std::max(first.arr.size(), second.arr.size());
 	int first_trit, second_trit;
 
-	TritArr res_arr(std::max(first.capacity(), second.capacity()));
+	TritSet res_arr(std::max(first.capacity(), second.capacity()));
 
 	for (int i = 0; i < maximum; i++) {
 		for (int j = 0; j < size_t; j++) {
 		
+			//read each trit
 			first_trit = first.read(i * size_t + j);
 			second_trit = second.read(i * size_t + j);
 			
+			//make new trit which is a result of operation
 			Trit new_tritf = static_cast <Trit> (first_trit);
 			Trit new_trits = static_cast <Trit> (second_trit);
 			Trit res_trit = new_tritf | new_trits;
 
+			//write this trit to the new set of trits
 			res_arr.SetTrit(i * size_t + j, res_trit);
 		}
 	}
 	return res_arr;
 }
 
-TritArr operator~(TritArr &first) {
-	const int size_t = sizeof(unsigned int) * 4;
+TritSet operator~(TritSet &first) {
+	const int size = sizeof(unsigned int) * 4;
 
-	TritArr res_arr(first.capacity());
+	TritSet res_arr(first.capacity());
+
 	for (int i = 0; i < first.arr.size(); i++) {
-		for (int j = 0; j < size_t; j++) {
-			int first_trit = first.read(i * size_t + j);
+		for (int j = 0; j < size; j++) {
+			//read trit
+			int first_trit = first.read(i * size + j);
 
+			//make new trit which is a result of operation
 			Trit new_tritf = static_cast <Trit> (first_trit);
 			Trit res_trit = ~new_tritf;
-			res_arr.SetTrit(i * size_t + j, res_trit);
+			//write this trit to the new set of trits
+			res_arr.SetTrit(i * size + j, res_trit);
 		}
 	}
 	return res_arr;
