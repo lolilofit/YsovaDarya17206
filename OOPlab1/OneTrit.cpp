4ad4a1ad8c8dc9e2013750509f1bdfc6decebf18
Darya <33616442+lolilofit@users.noreply.github.com>
2018-10-03 19:44:24 +0700

    Update OneTrit.cpp

--- OOPlab1/OneTrit.cpp
+++ OOPlab1/OneTrit.cpp
@@ -1,29 +1,68 @@
-#include "OneTrit.h"
+#ifndef _TRITARR_
+#define _TRITARR_
+#include"OneTrit.h"
+#include<iostream>
+#include<vector>
+#include<unordered_map>
 
-//logic operators for one trit
+class TritSet
+{
+	//the number of original size
+	int count = 0;
+	//last true/false element
+	int maxlen;
+	std::vector <unsigned int> set;
 
-Trit operator &(Trit first, Trit second) {
-	if (first == Trit::False || second == Trit::False)
-		return Trit::False;
-	if (first == Trit::True && second == Trit::True)
-		return Trit::True;
-	return Trit::Unknown;
-}
+	int rest(int pos);
 
-Trit operator |(Trit first, Trit second) {
-	if (first == Trit::True || second == Trit::True)
-		return Trit::True;
-	if (first == Trit::False && second == Trit::False)
-		return Trit::False;
-	return Trit::Unknown;
+	void _resize(int index);
 
-}
+ public:
+	 //set of trits
 
-Trit operator ~(Trit elem) {
-	if (elem == Trit::True)
-		return Trit::False;
-	if (elem == Trit::False)
-		return Trit::True;
-	return Trit::Unknown;
+	class SetProxy {
+	 public:
+		 TritSet * p;
+		 //position we want to set to or read from
+		int pos = -1;
+		//value in this position
+		int value = -1;
 
-}
+		SetProxy(int position, int val);
+
+		//Equal(const Equal &src) : pos(src.pos), value(src.value);
+
+		TritSet& operator=(const Trit &val);
+
+		operator Trit() const;
+	};
+	
+public:
+
+	std::unordered_map< Trit, int, std::hash<Trit> > cardinality();
+
+	TritSet(int length);
+
+	~TritSet();
+
+	void trim(int last_index);
+
+	size_t lenght();
+
+	size_t cardinality(Trit val);
+	
+	size_t capacity();
+
+	void shrink();
+
+	int read(int pos) const;
+
+	void set_trir(int pos, Trit val);
+
+	TritSet::SetProxy operator[](int pos);
+
+};
+
+bool operator==(TritSet::SetProxy trit_equal, Trit val);
+
+#endif
