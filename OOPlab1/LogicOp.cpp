c2d9f471fd2e55afacae07ee49e3532a6884d16b
Darya <33616442+lolilofit@users.noreply.github.com>
2018-10-03 19:43:47 +0700

    Update LogicOp.cpp

--- OOPlab1/LogicOp.cpp
+++ OOPlab1/LogicOp.cpp
@@ -9,7 +9,7 @@ TritSet operator&(TritSet &first, TritSet &second) {
 	const int size = sizeof(unsigned int) * 4;
 	
 	//size of new trit
-	int minimum = std::min(first.arr.size(), second.arr.size());
+	int minimum = std::min(first.lenght(), second.lenght());
 	TritSet res_arr(std::min(first.capacity(), second.capacity()));
 
 	for (int i = 0; i < minimum; i++) {
@@ -24,7 +24,7 @@ TritSet operator&(TritSet &first, TritSet &second) {
 			Trit res_trit = new_tritf & new_trits;
 
 			//write this trit to the new set of trits
-			res_arr.SetTrit(i * size + j, res_trit);
+			res_arr.set_trir(i * size + j, res_trit);
 		}
 	}
 	return res_arr;
@@ -34,7 +34,7 @@ TritSet operator|(TritSet &first, TritSet &second) {
 	const int size_t = sizeof(unsigned int) * 4;
 	
 	//size of new trit
-	int maximum = std::max(first.arr.size(), second.arr.size());
+	int maximum = std::max(first.lenght(), second.lenght());
 	int first_trit, second_trit;
 
 	TritSet res_arr(std::max(first.capacity(), second.capacity()));
@@ -52,7 +52,7 @@ TritSet operator|(TritSet &first, TritSet &second) {
 			Trit res_trit = new_tritf | new_trits;
 
 			//write this trit to the new set of trits
-			res_arr.SetTrit(i * size_t + j, res_trit);
+			res_arr.set_trir(i * size_t + j, res_trit);
 		}
 	}
 	return res_arr;
@@ -63,7 +63,7 @@ TritSet operator~(TritSet &first) {
 
 	TritSet res_arr(first.capacity());
 
-	for (int i = 0; i < first.arr.size(); i++) {
+	for (int i = 0; i < first.lenght(); i++) {
 		for (int j = 0; j < size; j++) {
 			//read trit
 			int first_trit = first.read(i * size + j);
@@ -72,7 +72,7 @@ TritSet operator~(TritSet &first) {
 			Trit new_tritf = static_cast <Trit> (first_trit);
 			Trit res_trit = ~new_tritf;
 			//write this trit to the new set of trits
-			res_arr.SetTrit(i * size + j, res_trit);
+			res_arr.set_trir(i * size + j, res_trit);
 		}
 	}
 	return res_arr;
